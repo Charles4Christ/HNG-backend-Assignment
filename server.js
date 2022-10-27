435eca54d1e8ec7af67965f0af06fdb10f59eb4a
Ogonnaya Charles <ogonnayacn@gmail.com>
2022-10-27 22:03:51 +0100

    project added

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express()
+ let data = {
+    "slackUsername": "Ogonnayacn", 
+    "backend": true, 
+    "age": 26, 
+    "bio": "I am Ogonnaya charles Nnamdi from Nigeria and i am backend developer"
+ }
+app.use(express.json());
+ app.get('/', (req, res) => {
+    res.status(200).json({ data })
+ })
+
+ const port = 3000
+
+ const start = async () => {
+    try {
+        app.listen(port, () =>
+          console.log(`Server is listening on port ${port}...`)
+        );
+      } catch (error) {
+        console.log(error);
+      }
+ }
+
+ start()
