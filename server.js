7b5dafa9ee2da3715c2e485d10748c6ae70f386d
Ogonnaya Charles <ogonnayacn@gmail.com>
2022-11-01 10:28:59 +0100

    I change the response

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 require('dotenv').config()
 const express = require('express');
 const app = express()
- let data = {
-    "slackUsername": "Ogonnayacn", 
-    "backend": true, 
-    "age": 26, 
-    "bio": "I am Ogonnaya charles Nnamdi from Nigeria and i am backend developer"
- }
+//  let data = {
+//     "slackUsername": "Ogonnayacn", 
+//     "backend": true, 
+//     "age": 26, 
+//     "bio": "I am Ogonnaya charles Nnamdi from Nigeria and i am backend developer"
+//  }
 app.use(express.json());
  app.get('/', (req, res) => {
-    res.status(200).json({ data })
+    res.status(200).json({ "slackUsername": "Ogonnayacn", 
+    "backend": true, 
+    "age": 26, 
+    "bio": "I am Ogonnaya charles Nnamdi from Nigeria and i am backend developer" })
  })
 
  const port = process.env.PORT || 3000
